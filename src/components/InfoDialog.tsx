448113f52c35269c765702de204b5ad4a99a35bd
Lisa <ll@webpunks.co>
2021-03-08 10:18:43 +0100

    Styled Info Dialog

--- src/components/InfoDialog.tsx
+++ src/components/InfoDialog.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./InfoDialog.scss";
 import { Dialog } from "./Dialog";
-import Stack from "./Stack";
 export const InfoDialog = ({ onClose }: { onClose?: () => void }) => {
   const handleClose = React.useCallback(() => {
     if (onClose) {
@@ -16,79 +15,70 @@ export const InfoDialog = ({ onClose }: { onClose?: () => void }) => {
         className={"InfoDialog"}
       >
         <div className="InfoDialog">
-          <Stack.Col align="center">
-            <Stack.Row>
-              Mit »Excalidraw« kannst du Vorgänge in einem Flussdiagramm
-              veranschaulichen, kollaborativ Ideen in einem Brainstorming
-              sammeln und Sketchnotes erstellen.
-            </Stack.Row>
-            <Stack.Row>
-              <button
-                type="button"
-                className="infoDialogButton"
-                onClick={() => handleClose()}
-                title="Weiter"
+          Mit »Excalidraw« kannst du Vorgänge in einem Flussdiagramm
+          veranschaulichen, kollaborativ Ideen in einem Brainstorming sammeln
+          und Sketchnotes erstellen.
+          <div>
+            <button
+              type="button"
+              className="infoDialogButton"
+              onClick={() => handleClose()}
+              title="Weiter"
+            >
+              Weiter
+            </button>
+          </div>
+          <div className="kitsFooter">
+            <a
+              href="https://kits.blog/tools/"
+              target="_blank"
+              rel="noreferrer"
+              title="Zurück zur Tools-Seite"
+            >
+              <img
+                className="InfoDialogKitsLogo"
+                alt="kits"
+                src="./kits-logo.svg"
+              />
+            </a>
+
+            <div className="InfoDialogLinks">
+              <ul>
+                <li>
+                  <a
+                    href="https://kits.blog/impressum/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Impressum
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://kits.blog/datenschutzerklaerung/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Datenschutz
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="InfoDialogGitlab">
+              <img
+                className="GitlabLink"
+                alt="gitlab_logo.svg"
+                src="./gitlab-logo.svg"
+              />
+              <a
+                href="https://gitlab.com/kits-apps/excalidraw"
+                rel="noreferrer"
+                target="_blank"
               >
-                Weiter
-              </button>
-            </Stack.Row>
-            <Stack.Row>
-              <div>
-                <a
-                  href="https://kits.blog/tools/"
-                  target="_blank"
-                  rel="noreferrer"
-                  title="Zurück zur Tools-Seite"
-                >
-                  <img
-                    className="InfoDialogKitsLogo"
-                    alt="kits"
-                    src="./kits-logo.svg"
-                  />
-                </a>
-              </div>
-            </Stack.Row>
-            <Stack.Row>
-              <div className="InfoDialogLinks">
-                <ul>
-                  <li>
-                    <a
-                      href="https://kits.blog/impressum/"
-                      rel="noreferrer"
-                      target="_blank"
-                    >
-                      Impressum
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://kits.blog/datenschutzerklaerung/"
-                      rel="noreferrer"
-                      target="_blank"
-                    >
-                      Datenschutz
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </Stack.Row>
-            <Stack.Row>
-              <div className="InfoDialogGitlab">
-                <img
-                  className="GitlabLink"
-                  alt="gitlab_logo.svg"
-                  src="./gitlab-logo.svg"
-                />
-                <a
-                  href="https://gitlab.com/kits-apps/excalidraw"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  Excalidraw
-                </a>
-              </div>
-            </Stack.Row>
-          </Stack.Col>
+                Excalidraw
+              </a>
+            </div>
+          </div>
         </div>
       </Dialog>
     </>
