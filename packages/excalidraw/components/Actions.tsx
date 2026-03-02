d09f3255a916b1a3c080779712615547e4b9df64
JannikStreek <jannik.streek@gmail.com>
2026-03-02 19:29:23 +0100

    translate missing strings

--- packages/excalidraw/components/Actions.tsx
+++ packages/excalidraw/components/Actions.tsx
@@ -1251,17 +1251,23 @@ export const ShapesSwitcher = ({
               {t("toolBar.lasso")}
             </DropdownMenu.Item>
           )}
-          <div style={{ margin: "6px 0", fontSize: 14, fontWeight: 600 }}>
-            Generieren
-          </div>
+          {app.props.aiEnabled !== false && (
+            <div style={{ margin: "6px 0", fontSize: 14, fontWeight: 600 }}>
+              Generieren
+            </div>
+          )}
           {app.props.aiEnabled !== false && <TTDDialogTriggerTunnel.Out />}
-          <DropdownMenu.Item
-            onSelect={() => app.setOpenDialog({ name: "ttd", tab: "mermaid" })}
-            icon={mermaidLogoIcon}
-            data-testid="toolbar-embeddable"
-          >
-            {t("toolBar.mermaidToExcalidraw")}
-          </DropdownMenu.Item>
+          {app.props.aiEnabled !== false && (
+            <DropdownMenu.Item
+              onSelect={() =>
+                app.setOpenDialog({ name: "ttd", tab: "mermaid" })
+              }
+              icon={mermaidLogoIcon}
+              data-testid="toolbar-embeddable"
+            >
+              {t("toolBar.mermaidToExcalidraw")}
+            </DropdownMenu.Item>
+          )}
           {app.props.aiEnabled !== false && app.plugins.diagramToCode && (
             <DropdownMenu.Item
               onSelect={() => app.onMagicframeToolSelect()}
